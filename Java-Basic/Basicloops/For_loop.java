03980ac216626ef84e2281cbce4bb4a9579e1369
maureenwaitherero <waithereromaureen@gmail.com>
2015-09-16 07:56:05 +0300

    loop files

---
+++ Java-Basic/Basicloops/For_loop.java
@@ -0,0 +1,12 @@
+package Basicloops;
+
+public class Forloop {
+	
+	 public static void main(String[] args){
+         for(int x=1; x<=20; x++){
+              System.out.println(x);
+         }
+    }
+	
+
+}
---
+++ Java-Basic/Basicloops/while_loop.java
@@ -0,0 +1,11 @@
+
+
+public class while_loop {
+	public static void main(String[] args){
+        int x = 1;
+        while (x <=20) {
+            System.out.println(x);
+            x++;
+        }
+    }
+}
